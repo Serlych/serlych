f0ea6b82d33d1666cd498485fa22bb9993d408e7
Isaac Chavoya <joelchavoya@hotmail.com>
2026-05-05 18:22:38 -0600

    fix(db): remove outdated drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,6 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
+  dialect: "postgresql",
   schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["*"],
 } satisfies Config;
