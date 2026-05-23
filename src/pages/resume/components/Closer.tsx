7e46e084b1b078a40d25f29960a842d3d6698bb1
Isaac Chavoya <joelchavoya@hotmail.com>
2026-05-22 21:49:18 -0600

    fix(closer): make text not cut

--- src/pages/resume/components/Closer.tsx
+++ src/pages/resume/components/Closer.tsx
@@ -9,7 +9,7 @@ export default function Closer() {
   return (
     <Section className="items-center">
       <div className="flex flex-col justify-center gap-5">
-        <h2 className="bg-gradient-to-l from-sky-400 to-violet-500 bg-clip-text text-3xl font-bold text-transparent sm:text-4xl lg:text-5xl">
+        <h2 className="inline-block break-words bg-gradient-to-l from-sky-400 to-violet-500 bg-clip-text pb-2 text-3xl font-bold leading-[1.1] text-transparent sm:text-4xl lg:text-5xl">
           {resumeData.closer.title}
         </h2>
         <h3 className="text-2xl sm:text-3xl">
